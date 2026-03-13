fa3f7a83754f5e7f5c879a563eb4b8c42624ad6e
Rajat <rajatbr13@gmail.com>
2026-03-13 21:26:29 +0530

    Calculator

---
+++ fastapi_basics/calculator.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI , HTTPException
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"Message" : "Welcome to the calculator",
+            "Usage" : "/calculate?operation=mode&num1=?&num2=?",
+            "modes" : "[add, sub, multiply, divide]"}
+
+@app.get("/calculate")
+def calculate(operation : str, num1 : float , num2 : float):
+    if operation == "add":
+        result = num1 + num2
+    elif operation == "sub":
+        result = num1 - num2
+    elif operation == "multiply":
+        result = num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            raise HTTPException(status_code = 400, detail = "Cannot divide number by 0 !")
+        result = num1 / num2
+    else:
+        raise HTTPException(status_code = 400, detail = "Invalid Input !")
+    
+    return {"Operation" : operation, 
+            "num1" : num1,
+            "num2" : num2, 
+            "Result" : result} 
+
+
