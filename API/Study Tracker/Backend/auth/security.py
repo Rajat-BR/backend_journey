3bc246a812d2ea8a9d6b2481328b76b4d9913fac
Rajat <rajatbr13@gmail.com>
2026-06-24 14:49:02 +0530

    define a function to create access token

--- API/Study Tracker/Backend/auth/security.py
+++ API/Study Tracker/Backend/auth/security.py
@@ -1,13 +1,34 @@
+import secrets
 from passlib.context import CryptContext
 from jose import jwt
+from datetime import timedelta, datetime, timezone
+
 
 pwd_context = CryptContext(
     schemes=["bcrypt"],
     deprecated="auto"
 )
 
+SECRET_KEY = secrets.token_hex(32)
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
 def hash_password(password: str):
-    return pwd_context.hash()
+    return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict):
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode = {
+        **data,
+        "exp": expire
+    }
+    return jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+    
